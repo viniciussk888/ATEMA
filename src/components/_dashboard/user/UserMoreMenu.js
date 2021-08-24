3cb456990235581c0d507ba3e91ca8e9e2d5be95
C1321518 Alberto Vinicius Martins Rocha <c1321518@interno.bb.com.br>
2021-08-24 17:22:05 -0300

    feat: modal

--- src/components/_dashboard/user/UserMoreMenu.js
+++ src/components/_dashboard/user/UserMoreMenu.js
@@ -1,9 +1,12 @@
 import { Icon } from '@iconify/react';
 import { useRef, useState } from 'react';
 import eyeFill from '@iconify/icons-eva/eye-fill';
-import { Link as RouterLink } from 'react-router-dom';
 import trash2Outline from '@iconify/icons-eva/trash-2-outline';
 import moreVerticalFill from '@iconify/icons-eva/more-vertical-fill';
+import Modal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import { makeStyles } from '@material-ui/styles';
+import Table from 'react-bootstrap/Table';
 // material
 import { Menu, MenuItem, IconButton, ListItemIcon, ListItemText } from '@material-ui/core';
 import { useSelector } from 'react-redux';
@@ -11,12 +14,46 @@ import apiAtema from '../../../services/apiAtema';
 
 // ----------------------------------------------------------------------
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}));
+
 export default function UserMoreMenu({ atemaId }) {
+  const classes = useStyles();
   const ref = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
 
   const config = {
     headers: { Authorization: `Bearer ${useSelector((state) => state.token)}` }
+  };
+
+  async function getData() {
+    if (data.length === 0) {
+      const response = await apiAtema.get(`atema/${atemaId}`, config);
+      setData(response.data);
+      console.warn(response.data);
+    }
+  }
+
+  const handleOpen = () => {
+    getData();
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   async function deleteData() {
@@ -41,6 +78,87 @@ export default function UserMoreMenu({ atemaId }) {
 
   return (
     <>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Informações Toponímicas</h2>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Mesorregião</th>
+                  <th>Microrregião</th>
+                  <th>Município</th>
+                  <th>Elemento Geográfico</th>
+                  <th>Topônimo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{data.mesorregiao}</td>
+                  <td>{data.microrregiao}</td>
+                  <td>{data.municipio}</td>
+                  <td>{data.elementogeografico}</td>
+                  <td>{data.toponimo}</td>
+                </tr>
+              </tbody>
+              <thead>
+                <tr>
+                  <th>Tipo</th>
+                  <th>Área</th>
+                  <th>Língua de Origem</th>
+                  <th>Taxionomia</th>
+                  <th>Estrutura Morfológica</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{data.tipo}</td>
+                  <td>{data.area}</td>
+                  <td>{data.linguaOrigem}</td>
+                  <td>{data.taxionomia}</td>
+                  <td>{data.estruturaMorfologica}</td>
+                </tr>
+              </tbody>
+              <thead>
+                <tr>
+                  <th>Referências</th>
+                  <th>Fonte (dados do mapa)</th>
+                  <th>Data da coleta</th>
+                  <th>Responsavel pela coleta</th>
+                  <th>Revisor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{data.referencias}</td>
+                  <td>{data.fonte}</td>
+                  <td>{data.dataColeta}</td>
+                  <td>{data.responsavel}</td>
+                  <td>{data.revisor}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <div>
+              <strong>Variante</strong>
+              <p>{data.variante}</p>
+              <strong>Etimologia</strong>
+              <p>{data.etimologia}</p>
+              <strong>Etimologia</strong>
+              <p>{data.etimologiaEsc}</p>
+              <strong>Observações</strong>
+              <p>{data.observacoes}</p>
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+
       <IconButton ref={ref} onClick={() => setIsOpen(true)}>
         <Icon icon={moreVerticalFill} width={20} height={20} />
       </IconButton>
@@ -55,11 +173,11 @@ export default function UserMoreMenu({ atemaId }) {
         anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       >
-        <MenuItem component={RouterLink} to="#" sx={{ color: 'text.secondary' }}>
+        <MenuItem onClick={handleOpen} sx={{ color: 'text.secondary' }}>
           <ListItemIcon>
             <Icon icon={eyeFill} width={24} height={24} />
           </ListItemIcon>
-          <ListItemText primary="Ver mais" primaryTypographyProps={{ variant: 'body2' }} />
+          <ListItemText primary="Ver informações" primaryTypographyProps={{ variant: 'body2' }} />
         </MenuItem>
 
         <MenuItem onClick={deleteData} sx={{ color: 'text.secondary' }}>
